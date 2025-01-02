88e80aacef5cb4f34b78605373db1309c98af5b8
feyzann <122739488+feyzann@users.noreply.github.com>
2025-01-02 03:18:36 +0300

    son degisiklikler

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using AspNetCoreHero.ToastNotification.Abstractions;
 using IleriWebProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace IleriWebProject.Controllers
 {
@@ -20,14 +21,54 @@ namespace IleriWebProject.Controllers
 
         public IActionResult Index()
         {
-            //var books = _libraryManagementSystemContext.Books.ToList(); EntityFramework linq sorgularý
-            return View();
+            // Kitaplarý çekiyoruz
+            var books = _libraryManagementSystemContext.Books
+                .Include(b => b.Category)  // Kitaplarýn kategorisini de dahil ediyoruz
+                .ToList();
+
+            // Eðer kitap yoksa, hata mesajý gösterelim
+            if (books == null || !books.Any())
+            {
+                _notyf.Error("Hiç kitap bulunamadý.");
+                return View(new List<Book>());
+            }
+
+            return View(books);
         }
 
         public IActionResult Privacy()
         {
             return View();
         }
+
+        public IActionResult BorrowBook(int bookId)
+        {
+            var book = _libraryManagementSystemContext.Books
+                .FirstOrDefault(b => b.BookId == bookId);
+
+            if (book == null)
+            {
+                _notyf.Error("Kitap bulunamadý.");
+                return RedirectToAction("Index");
+            }
+
+            // Kiralama iþlemi yapýlabilir
+            // Örneðin, BorrowTransaction kaydýný ekleyelim
+            var transaction = new Borrowtransaction
+            {
+                UserId = 1, // Örnek kullanýcý id'si. Gerçek kullanýcý id'si buraya eklenmeli.
+                BookId = book.BookId,
+                BorrowDate = DateOnly.FromDateTime(DateTime.Now),
+            };
+
+            _libraryManagementSystemContext.Borrowtransactions.Add(transaction);
+            _libraryManagementSystemContext.SaveChanges();
+
+            _notyf.Success("Kitap baþarýyla kiralandý!");
+
+            return RedirectToAction("Index");
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
